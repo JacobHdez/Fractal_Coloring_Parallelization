c0b96b2e623608620e613fdeef7cb8b32dbfd32a
Jacobo Hernandez Varela <jacohv98@gmail.com>
2020-04-17 21:03:41 -0500

    Add spline header

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,10 @@
 #include "headers/rgb.h"
 #include "headers/image.h"
+#include "spline.h"
 #include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Hello, World!" << endl;
-
-    RGB fondo(0.5, 0.25, 0.25);
-    Image test(128, 128, fondo);
-
-    savePPM(test, "../results/test_img.ppm");
-
-    cout << "Hello, World!" << endl;
     return 0;
 }
